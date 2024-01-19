76a58901d4126cc93ca53e3e5189f9a35c85e67e
Nico44YT <Nico_44-YouTube@gmx.de>
2024-01-19 21:32:46 +0100

    Initial Commit

---
+++ src/main/java/net/letscode/biomesobark/BiomesOBark.java
@@ -0,0 +1,27 @@
+package net.letscode.biomesobark;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BiomesOBark.MOD_ID)
+public class BiomesOBark {
+    public static final String MOD_ID = "biomes_o_bark";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public BiomesOBark() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+}
